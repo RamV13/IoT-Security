6cd266726c7eb41d0215c7844c8616195c44193f
ramv13 <ramsai.vellanki@gmail.com>
2017-11-27 20:18:19 -0500

    Updating server to buffer up content

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ app.use(bodyParser.json());
 var net = require('net');
 
 var fs = require('fs');
+var lineReader = require('readline');
 
 var _ = require('lodash');
 
@@ -24,32 +25,36 @@ var playing = false;
 // SOCKET
 
 const CHUNK_SIZE = 1; // TODO calculate optimal chunk size
+const PLAYING_DELAY = 1000;
+const DATA_DELAY = 0;
 
 /**
  * Retrieve an array of data bytes from a WAV file given a song in a format
  * suitable for a 12-bit DAC
  */
 function get_song_bytes(song, callback) {
-  fs.readFile('./songs/' + song, (err, data) => {
-    if (err) return callback(null, error('failed to read song: ' + song));
-    data = _.split(data, ',');
-    callback(data);
-  });
+  var buffer = [];
+  lineReader
+    .createInterface({input: fs.createReadStream('./songs/' + song)})
+    .on('line', (line) => {
+      buffer.push(line);
+      if (_.size(buffer) == CHUNK_SIZE) {
+        callback(buffer);
+        buffer = [];
+      }
+    });
 }
 
 var server = net.createServer((socket) => {
   socket.setEncoding('utf8');
   socket.on('data', (data) => {
     get_song_bytes(_.trimEnd(data), (data, err) => {
-      if (err) {
-        socket.write('failure');
-      } else {
-        function send_data(data) {
-          if (playing) socket.write('' + _.head(_.head(data)));
-          if (_.size(data) > 1) setTimeout(() => { send_data(playing ? _.tail(data) : data); }, 0);
-        }
-        send_data(_.chunk(data, CHUNK_SIZE));
+      if (err) return console.error('song byte conversion failure');
+      function send_data(data) {
+        if (playing) socket.write('' + _.head(data));
+        if (_.size(data) > 1) setTimeout(() => { send_data(playing ? _.tail(data) : data); }, playing ? DATA_DELAY : PLAYING_DELAY);
       }
+      send_data(data);
     });
   });
 });
