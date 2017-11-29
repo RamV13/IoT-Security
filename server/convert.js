f2299d872afcd18928240aae297900da7cde524a
ramv13 <ramsai.vellanki@gmail.com>
2017-11-28 21:20:09 -0500

    Updating convert script to modify data format

--- server/convert.js
+++ server/convert.js
@@ -27,5 +27,36 @@ var data = result.channelData[0];
 var min = _.min(data);
 var max = _.max(data);
 data = _.map(data, (x) => Math.trunc((x - min) * 4095.5 / (max - min)));
-// TODO bitwise OR with DAC CS
+
+function int2binary(num) {
+  var bits = [];
+  for (var i = 0; i < 12; i++) {
+    bits.push(num % 2);
+    num = num >> 1;
+  }
+  return _.reverse(bits);
+}
+
+function binary2int(bits) {
+  bits = _.reverse(bits);
+  var num = 0;
+  for (var i = 0; i < _.size(bits); i++) {
+    num += bits[i] * Math.pow(2, i);
+  }
+  return num;
+}
+
+var count = 0;
+
+data = _.map(data, (x) => {
+  var bits = int2binary(x);
+  var least = _.slice(bits, 6, 12);
+  least.unshift(0);
+  least = binary2int(least);
+  var most = _.slice(bits, 0, 6);
+  most.unshift(1);
+  most = binary2int(most);
+  return least + "," + most;
+});
+
 fs.writeFileSync(song + 's', _.reduce(data, (acc, x) => acc + x + '\n', ''));
